10cdfb4ae37b8873a51852acb40077d3a545e26a
BountifulRaptorAdam <18ethanquarry@kinsalecommunityschool.ie>
2023-07-24 00:19:18 +0100

    Cleanup

--- app/dashboard/budgets/page.tsx
+++ app/dashboard/budgets/page.tsx
@@ -79,9 +79,9 @@ export default async function BudgetsPage() {
       <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <h2 className="text-2xl font-bold tracking-tight">Expense Insights Manager</h2>
             <p className="text-muted-foreground">
-              Here&apos;s a list of your Bills for this month!
+              Maintain and Control your Expenses 🌧
             </p>
           </div>
           <div className="flex items-center space-x-2">
--- app/dashboard/income/page.tsx
+++ app/dashboard/income/page.tsx
@@ -75,9 +75,9 @@ export default async function BudgetsPage() {
       <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <h2 className="text-2xl font-bold tracking-tight">Income Stream Manager 💸</h2>
             <p className="text-muted-foreground">
-              Here&apos;s a list of your Bills for this month!
+            Organize, Track, and Optimize your Multiple Streams of Income.
             </p>
           </div>
           <div className="flex items-center space-x-2">
