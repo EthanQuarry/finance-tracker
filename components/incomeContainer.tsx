2ccfe0239c34ae537d61448ec2fb0260ec3963bd
BountifulRaptorAdam <18ethanquarry@kinsalecommunityschool.ie>
2023-07-24 22:24:33 +0100

    Broke it

--- components/incomeContainer.tsx
+++ components/incomeContainer.tsx
@@ -5,6 +5,7 @@ import IncomeTable from "./income/table";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import IncomeEdit from "./income/edit";
 
 type IncomeDataProps = {
   userId: string
@@ -32,81 +33,19 @@ type RowData = {
 
 
 export default function IncomeContainer({ data, userId }: IncomeDataProps) {
+  const [ rowId , setRowId ] = useState<EditRowProps>({
+    rowId: ''
+  })
+  
   const [rowData, setRowData] = useState<RowData>({
     id: '',
     name: '',
     amount: null,
     note: ''
   })
-  const [editRowBoolean, setEditRowBoolean] = useState(false)
 
-  const EditRow = async (rowId : EditRowProps) => {
-      const submit = async () => {
-        const response = await fetch('/api/user/income/put', {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ id: rowId, name: rowData.name, amount: rowData.amount, note: rowData.note })
-        })
-      }
-    return (
-      <>
-        <div className='gruserId lg:gruserId-cols-2'>
-
-          <Input
-            className='lg:cols-span-1 '
-            placeholder="Name"
-            type="text"
-            value={rowData.name}
-            onChange={(e) => setRowData({
-              id: rowData.id,
-              name: e.target.value,
-              amount: rowData.amount,
-              note: rowData.note
-            })}
-          />
-
-          <Input
-            className='lg:cols-span-1'
-            placeholder="Amount assigned"
-            type="number"
-            value={rowData.amount?.toString()}
-            onChange={(e) => setRowData({
-              id: rowData.id,
-              name: rowData.name,
-              amount: parseInt(e.target.value),
-              note: rowData.note
-            })}
-          />
-
-        </div>
-        <div className='gruserId lg:gruserId-cols-2'>
-          <Input
-            className='lg:cols-span-2'
-            placeholder="Little Note"
-            type='text'
-            value={rowData.note}
-            onChange={(e) => setRowData({
-              id: rowData.id,
-              name: rowData.name,
-              amount: rowData.amount,
-              note: e.target.value
-            })}
-          />
-
-        </div>
-        <Button onClick={submit}>Submit</Button>
-      </>
-    )
-  }
-
-  const EditContainer = () => {
-    if (editRowBoolean) {
-      return (
-        <EditRow rowId={""}/>
-      )
-    } else return <div>it is not true</div>
+  const editRow = (rowId: string) => {
+    return
   }
 
 
@@ -114,15 +53,15 @@ export default function IncomeContainer({ data, userId }: IncomeDataProps) {
     <>
       <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-3 sm:grid-cols-1">
         <Card className="md:col-span-1 lg:col-span-2 ">
-          <IncomeTable data={data} EditRow={EditRow} setEditRowBoolean={setEditRowBoolean} />
+          <IncomeTable editRow={editRow} data={data}/>
         </Card>
         <Card className="md:col-span-1 lg:col-span-1">
           <CardHeader>
-            Edit Income
+            Add Income Source
           </CardHeader>
 
           <CardContent>
-            <EditContainer />
+            <IncomeEdit editRow={editRow} rowId={""}  userId={userId}/>
           </CardContent>
         </Card>
       </div>
