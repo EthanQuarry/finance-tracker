302b1f6ebc16cfc1a4d3e8dda88c40fb9d5f58a8
Ethan <18ethanquarry@kinsalecommunityschool.ie>
2023-07-27 08:00:42 +0100

    URL Env

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,8 +7,7 @@ import { getIdFromCookie } from "@/lib/auth";
 export const getData = async () => {
   const id = await getIdFromCookie(cookies())
 
-  // for server component requests absolute url required
-  const res = await fetch(`http://localhost:3000/api/user/getUserById`, {
+  const res = await fetch(`${process.env.URL}/api/user/getUserById`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
