a68b492da84a79ade29ca56f64c5345c2d4ecf89
Ethan <18ethanquarry@kinsalecommunityschool.ie>
2023-08-03 01:03:00 +0200

    tailwind custom screen sizes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="flex md:grid items-center justify-center sm:pt-20 lg:max-w-none lg:h-full lg:grid-cols-2 lg:px-0">
         <Link
           href="/signup"
           className={cn(
@@ -40,7 +40,7 @@ export default function AuthenticationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-8 sm:pt-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
