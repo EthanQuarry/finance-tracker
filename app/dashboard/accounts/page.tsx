60330ee056e9781f6753e04333644ea99d8cb1aa
BountifulRaptorAdam <18ethanquarry@kinsalecommunityschool.ie>
2023-08-16 00:46:58 +0100

    test

--- app/dashboard/accounts/page.tsx
+++ app/dashboard/accounts/page.tsx
@@ -1,5 +1,8 @@
 export default function Accounts() {
     return (
-        <div></div>
+        <div className="grid-cols-1 w-screen">
+            <h5 className="col-span-1 place-items-center text-center h-full m-64">Currently in Development</h5>
+        </div>
+
     )
 }
