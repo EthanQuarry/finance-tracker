b626083859dff5644a24fd21bb0cd3b8547e723c
BountifulRaptorAdam <18ethanquarry@kinsalecommunityschool.ie>
2023-08-15 23:53:42 +0100

    big refactor

--- pages/api/user/expenses/sub/get.js
+++ pages/api/user/expenses/sub/get.js
@@ -3,15 +3,20 @@ import { db } from '@/lib/db';
 export default async function handler(req, res) {
   if (req.method === 'POST') {
 
-    const expenses = await db.expense.findMany({
+
+
+    const categories = await db.expense.findMany({
       where: {
         userId: req.body.userId
+      },
+      include: {
+        unique: true
       }
     })
 
-    if (expenses.ok) {
+    if (categories.ok) {
       return res.status(404).json({ message: 'Categories not found' })
     }
-    return res.status(200).json(expenses)
+    return res.status(200).json(categories)
   }
 }
---
+++ pages/api/user/expenses/sub/post.js
@@ -0,0 +1,30 @@
+import { db } from '@/lib/db';
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+
+                 const expense = await db.expense.create({
+                data: {
+                    name: req.body.name,
+                    amount: req.body.amount,
+                    note: req.body.note,
+                    user: {
+                         connect: {
+                            id: req.body.userId
+                    }
+                    }
+                   
+                }
+            })
+        
+           
+
+
+
+            if (expense.ok) {
+                return res.status(500).json({ message: 'No' })
+            } else return res.status(200).json({ user: expense })
+
+        }
+
+    }
