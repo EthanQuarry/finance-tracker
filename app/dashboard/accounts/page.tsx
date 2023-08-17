ddad5856002a65ee16f28234979bb56843209727
BountifulRaptorAdam <18ethanquarry@kinsalecommunityschool.ie>
2023-08-17 14:04:44 +0100

    account fixed margin

--- app/dashboard/accounts/page.tsx
+++ app/dashboard/accounts/page.tsx
@@ -1,7 +1,7 @@
 export default function Accounts() {
     return (
         <div className="grid-cols-1 w-screen">
-            <h5 className="col-span-1 place-items-center text-center h-full m-64">Currently in Development</h5>
+            <h5 className="col-span-1 place-items-center text-center h-full">Currently in Development</h5>
         </div>
 
     )
